ca1b8ee7ca7b2f4ccb870cf446784587e26fbf27
Edmundo Andrade <edmon.af@gmail.com>
2020-02-17 23:35:53 -0600

    code unused

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,6 @@ export default function Index() {
             {formatMessage("menu.home")}
           </Typography>
         </Box>
-
-         <p>{process.env.SECRET}</p>
       </Container>
     </Layout>
   );
