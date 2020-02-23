5794c19c24643edf421da72a12acb659d5851116
Edmundo Andrade <edmon.af@gmail.com>
2020-02-23 11:59:45 -0600

    change: manifest.json and images. new data: contact

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,5 +1,6 @@
 export const contactItems = [ 
     { title: "name", required: true, multiline:false, defaultValue: ""},
     { title: "email", required: true, multiline: false, defaultValue: "" },
+    { title: "phone", required: false, multiline: false, defaultValue: "" },
     { title: "message", required: true, multiline: true, defaultValue: "" }
   ]
